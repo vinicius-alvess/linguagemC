8ccc3c5a10f473678faf961fe4ab19fbdff37671
Vinícius Alves <viniciusalves999@gmail.com>
2021-03-10 19:19:37 -0300

    Atualização
    Reorganizei o código para que rode um apenas um arquivo .c. Porém é simples separar em arquivos .c separados.

--- linguagemC/pacman/fogefoge.c
+++ linguagemC/pacman/fogefoge.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fogefoge.h"
+#include "mapa.h"
 
-char** mapa;
-int linhas;
-int colunas;
+MAPA m;
+POSICAO heroi;
 
-void lemapa() {
+/*arquivo mapa.c*/
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "mapa.h"
+void lemapa(MAPA* m) {
 	FILE* f;
 	f = fopen("mapa.txt", "r");
 	if(f == 0) {
@@ -14,52 +18,96 @@ void lemapa() {
 		exit(1);
 	}
 
-	fscanf(f, "%d %d", &linhas, &colunas);
-	alocamapa();
+	fscanf(f, "%d %d", &(m->linhas), &(m->colunas));
+	alocamapa(m);
 	
-	for(int i = 0; i < 5; i++) {
-		fscanf(f, "%s", mapa[i]);
+	for(int i = 0; i < m->linhas; i++) {
+		fscanf(f, "%s", m->matriz[i]);
 	}
 
 	fclose(f);
 }
 
-void alocamapa() {
-	mapa = malloc(sizeof(char*) * linhas);
+void alocamapa(MAPA* m) {
+	m->matriz = malloc(sizeof(char*) * m->linhas);
 
-	for(int i = 0; i < linhas; i++) {
-		mapa[i] = malloc(sizeof(char) * colunas + 1);
+	for(int i = 0; i < m->linhas; i++) {
+		m->matriz[i] = malloc(sizeof(char) * m->colunas + 1);
 	}
 }
 
-void liberamapa() {
-	for(int i = 0; i < linhas; i++) {
-		free(mapa[i]);
+void liberamapa(MAPA* m) {
+	for(int i = 0; i < m->linhas; i++) {
+		free(m->matriz[i]);
 	}
 
-	free(mapa);
+	free(m->matriz);
 }
 
-move(){
+void imprimemapa(MAPA* m) {
+	for(int i = 0; i < m->linhas; i++) {
+		printf("%s\n", m->matriz[i]);
+	}
+}
 
+void encontramapa(MAPA* m, POSICAO* p, char c) {
+
+	for(int i = 0; i < m->linhas; i++) {
+		for(int j = 0; j < m->colunas; j++) {
+			if(m->matriz[i][j] == c) {
+				p->x = i;
+				p->y = j;
+				return;
+			}
+		}
+	}
+
+}
+/*FIM do arquivo mapa.c*/
+
+int acabou() {
+	return 0;
+}
+
+void move(char direcao) {
+
+	m.matriz[heroi.x][heroi.y] = '.';
+
+	switch(direcao) {
+		case 'a':
+			m.matriz[heroi.x][heroi.y-1] = '@';
+			heroi.y--;
+			break;
+		case 'w':
+			m.matriz[heroi.x-1][heroi.y] = '@';
+			heroi.x--;
+			break;
+		case 's':
+			m.matriz[heroi.x+1][heroi.y] = '@';
+			heroi.x++;
+			break;
+		case 'd':
+			m.matriz[heroi.x][heroi.y+1] = '@';
+			heroi.y++;
+			break;
+	}
+	
 }
 
 int main() {
 	
-	lemapa();
+	lemapa(&m);
+	encontramapa(&m, &heroi, '@');
 
-	move();
-do{
-	
-	imprimemapa();
+	do {
+		imprimemapa(&m);
 
-	char comando;
-	
-	for(int i = 0; i < linhas; i++) {
-		printf("%s\n", mapa[i]);
-	}
+		char comando;
+		scanf(" %c", &comando);
 
-}
+		move(comando);
 
-	liberamapa();
+	} while (!acabou());
+
+	liberamapa(&m);
 }
--- linguagemC/pacman/fogefoge.h
+++ linguagemC/pacman/fogefoge.h
@@ -1,11 +1,2 @@
-#ifndef fogefoge_h
-#define fogefoge_h
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void alocamapa();
-void lemapa();
-void liberamapa();
-
-#endif
+int acabou();
+void move(char direcao);
