945cd16b26c4a2c093de4965c1616ad38dc2ee99
viniciusalves999 <77307847+viniciusalves999@users.noreply.github.com>
2021-06-08 12:24:30 -0300

    Organizando

---
+++ linguagemCpp/adivinhacao/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+
+int main()
+{
+    // Mastra na tela std::cout << "Exemplo"
+    std::cout << "**************************************" << std::endl;
+    std::cout << "* Bem vindos ao jogo da adivinhação! *" << std::endl;
+    std::cout << "**************************************" << std::endl;
+}
