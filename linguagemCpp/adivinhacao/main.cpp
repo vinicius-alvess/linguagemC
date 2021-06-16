e140106984af25dae3fcc7968c41357fe052d1b4
viniciusalves999 <77307847+viniciusalves999@users.noreply.github.com>
2021-06-15 21:28:33 -0300

    Update main.cpp

--- linguagemCpp/adivinhacao/main.cpp
+++ linguagemCpp/adivinhacao/main.cpp
@@ -8,6 +8,6 @@ int main()
 
     // Mastra na tela std::cout << "Exemplo"
     std::cout << "**************************************" << std::endl;
-    std::cout << "* Bem vindos ao jogo da adivinhação! *" << std::endl;
+    std::cout << "* Bem vindos ao jogo da adivinhaÃ§Ã£o! *" << std::endl;
     std::cout << "**************************************" << std::endl;
 }
